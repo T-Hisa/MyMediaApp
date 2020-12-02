02e7d4c603ef397f3cc2ed69eea2299075593c8a
Takaaki <dacchin.h081229@gmail.com>
2020-12-02 22:55:41 +0900

    設定もれ

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    I18n.available_locales = [:ja, :en]
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
