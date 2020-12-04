7e25c4bc80640b565291d59be92be4a2cec4fb9c
Takaaki <dacchin.h081229@gmail.com>
2020-12-04 23:53:04 +0900

    install and set toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+// 参照した記事では、『toastrを使用するのにjqueryが必須』と書いてあったので、書いたが、どうやらなくても動作するようなので、コメントアウト。
+// require('jquery')
 
 import 'bootstrap';
-import '../stylesheets/application'
+import '../stylesheets/application';
 
+import toastr from 'toastr';
+window.toastr = toastr;
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+
+// jQueryとBootstapのJSを使えるように
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+    // Toastr: 'toastr'
+  })
+)
