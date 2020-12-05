89b09cb3d67390c012c162155cbeabdde75f0529
Takaaki <dacchin.h081229@gmail.com>
2020-12-06 02:16:10 +0900

    1. generate news create file(view). 2. arrange scss files.

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -7,9 +7,18 @@ class NewsController < ApplicationController
   end
 
   def new
+    @news = News
   end
 
   def create
+    news = News.new(news_params)
+    if news.save
+      flash[:success] = "#{news.title} の記事を作成しました"
+      redirect_to news_index_path
+    else
+      redirect_back(fallback_location: root_path)
+      flash[:error] = "#{news.errors.full_messages}"
+    end
   end
 
   def destroy
@@ -17,4 +26,9 @@ class NewsController < ApplicationController
 
   def update
   end
+
+  private
+    def news_params
+      params.require(:news).permit(:title, :content, :summary)
+    end
 end
