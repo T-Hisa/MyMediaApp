8dee91063e53db274a8478537be24d4368a839d5
Takaaki <dacchin.h081229@gmail.com>
2020-12-06 02:17:40 +0900

    insert new column to News Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_074843) do
+ActiveRecord::Schema.define(version: 2020_12_05_142516) do
 
   create_table "news", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_04_074843) do
     t.boolean "isDraft", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "summary"
   end
 
 end
