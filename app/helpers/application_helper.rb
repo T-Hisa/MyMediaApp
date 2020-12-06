465c521891a404b6f2f0b9de06dcaa71c4f6a7c8
Takaaki <dacchin.h081229@gmail.com>
2020-12-06 22:20:46 +0900

    ヘッダーリンク用の新しいヘルパーの追加 その他諸々の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def header_link(text, path)
+    class_name = 'nav-link'
+    class_name << ' disable active' if current_page?(path)
+    
+    link_to text, path, class: class_name
+  end
 end
