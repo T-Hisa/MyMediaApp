6576bdcc8841102e7653b11982a99824991160bf
Takaaki <dacchin.h081229@gmail.com>
2020-12-06 19:26:27 +0900

    updateアクションの整理

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,10 +1,12 @@
 class NewsController < ApplicationController
+  before_action :set_news, only: %i[show edit update]
+
+
   def index
     @news = News.all
   end
 
   def show
-    @news = News.find(params[:id])
   end
 
   def new
@@ -17,23 +19,31 @@ class NewsController < ApplicationController
       flash[:success] = "#{news.title} の記事を作成しました"
       redirect_to news_index_path
     else
-      redirect_back(fallback_location: root_path)
       flash[:error] = "#{news.errors.full_messages}"
+      redirect_back(fallback_location: root_path)
     end
   end
 
   def edit
-    @news = News.find(params[:id])
   end
 
   def destroy
   end
 
   def update
+    if News.update(news_params)
+      redirect_to @news
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   private
     def news_params
       params.require(:news).permit(:title, :content, :summary)
+    end
+
+    def set_news
+      @news = News.find(params[:id])
     end
 end
