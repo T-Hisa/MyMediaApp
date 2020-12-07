11f85f5f6b64ba351cab40081f61599d5ef5b1bd
Takaaki <dacchin.h081229@gmail.com>
2020-12-07 20:30:07 +0900

    issue#37 ログインしているかどうかでヘッダーに表示されるリンクを変更する change header_link by judgement of current_user id nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
     def current_user
-      @current_user = User.find_by(id: session[:user_id]) unless session[:user_id]
+      @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
     end
 end
