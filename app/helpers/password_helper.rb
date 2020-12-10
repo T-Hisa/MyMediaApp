922658e730e21deb459c08efdec6fdead2fbaeff
Takaaki <dacchin.h081229@gmail.com>
2020-12-11 06:43:45 +0900

    #52 i18n 対応1

--- app/helpers/password_helper.rb
+++ app/helpers/password_helper.rb
@@ -1,23 +1,10 @@
 module PasswordHelper
-  # こちらは念のためコメントアウトで残しておく
-
-  # def auto_password_field(form, type)
-  #   class_name = 'form-control'
-  #   # error_text の認証に使用するものと　viewに表記する『labe_text』が同一の文字列で十分なので、以下のように定義する。後に別々で定義にするかも？
-  #   label_text = type == :password ? 'パスワード' : 'パスワード(確認)'
-  #   class_name << ' border-danger' if error_text_include?(label_text)
-
-  #   tag.div class: 'form-group' do
-  #     concat form.label type, label_text
-  #     concat form.password_field type, class: class_name
-  #   end
-  # end
 
   def auto_password_field(form)
     tag.div do
       2.times do |i|
         concat (tag.div class: 'form-group' do
-          label_text = i == 0 ? 'パスワード' : 'パスワード(確認)'
+          label_text = i == 0 ? t('.password') : t('.password-confirmation')
           type = i == 0 ? :password : :password_confirmation
           class_name = 'form-control'
           class_name << ' border-danger' if error_text_include?(label_text)
@@ -34,7 +21,7 @@ module PasswordHelper
         concat (tag.div class: 'form-group' do
 
           label_text, flag, type, id_name = i == 0 ? 
-            ['新規パスワード', /パスワード.+入/ , :password, 'password'] : ['パスワード(確認)', /パスワード\(確認\)/, :password_confirmation, 'password_confirmation']
+            [t('.password'), /パスワード.+入/ , :password, 'password'] : [t('.password-confirmation'), /パスワード\(確認\)/, :password_confirmation, 'password_confirmation']
           class_name = 'form-control'
           class_name << ' border-danger' if error_text_include?(flag)
           concat form.label type, label_text
@@ -45,7 +32,7 @@ module PasswordHelper
   end
 
   def current_password_field_for_edit(form)
-    label_text = '現在のパスワード'
+    label_text = t('.current-password')
     class_name = 'form-control'
     class_name << ' border-danger' if error_text_include?(label_text)
     default_value = flash[:current_password]
