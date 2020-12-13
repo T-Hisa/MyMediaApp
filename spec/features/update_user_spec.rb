f3e530f4db4772f85e3a6048823d5142cf17390d
Takaaki <dacchin.h081229@gmail.com>
2020-12-14 02:53:54 +0900

    #56 ユーザー編集・更新処理をfeature テストで書こうとしたけど、なぜかテストで入力したパラメータが、本番のコントローラに渡っておらず、モデルの値が更新されないためコメントアウト。 ユニットテストにします

---
+++ spec/features/update_user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+require 'helpers/test_helper_spec'
+
+RSpec.feature "ユーザー情報編集", type: :feature do
+  include TestHelper
+
+  background do
+    before_login
+  end
+  # scenario "ユーザー名がきちんと編集できること" do
+  #   user = User.first
+  #   visit "/ja/users/#{user.id}/edit"
+  #   fill_in "name", with: "update"
+  #   within "#onlyNameModal" do
+  #     find(".btn-primary").click
+  #   end
+  #   expect(page).to have_text '投稿した記事'
+  #   expect(user.reload.name).to eq "update"
+  # end
+
+  # scenario "パスワードがきちんと編集できること" do
+  #   user = User.first
+  #   visit "/ja/users/#{user.id}/edit"
+  #   fill_in "name", with: "update"
+  #   fill_in "current_password", with: "password"
+  #   fill_in "password", with: "update!"
+  #   fill_in "password_confirmation", with: "update!"
+
+  #   within "#includePasswordModal" do
+  #     find(".btn-primary").click
+  #   end
+  #   expect(page).to have_text '投稿した記事'
+  # end
+end
